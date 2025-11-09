da935aa839c353c832c0a5800b618a5499c6613b
Naveen Prasanna <naveenprasannavnp@gmail.com>
2025-11-09 20:26:19 +0530

    Initial commit

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,77 @@
+package com.demo;
+
+class Car extends Thread {
+    public String createCar(int i) {
+        return "Car is running : " + i;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(createCar(i));
+        }
+    }
+}
+
+class Bus implements Runnable {
+
+    public String createBus(int i) {
+
+        return "Bus is running : " + i;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(createBus(i));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println(" --- "+Thread.currentThread().getName()+" --- ");
+
+        Car c = new Car();
+
+        c.setName("Thread-1");
+        c.start();
+        System.out.println("Thread : " + c.getName() + ", " + "is alive : " + c.isAlive());
+        c.yield();
+        System.out.println("Thread : " + c.getName() + ", " + "is alive : " + c.isAlive());
+
+        Thread t1 = new Thread(new Bus());
+
+        t1.setName("Thread-2");
+        t1.start();
+        System.out.println("Thread : " + t1.getName() + ", " + "is alive : " + t1.isAlive());
+        t1.yield();
+        System.out.println("Thread : " + t1.getName() + ", " + "is alive : " + t1.isAlive());
+
+        Thread t3 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Lorry is running : " + i);
+            }
+        },"Thread - 3");
+
+        t3.start();
+        System.out.println("Thread : " + t3.getName() + ", " + "is alive : " + t3.isAlive());
+        t3.yield();
+        System.out.println("Thread : " + t3.getName() + ", " + "is alive : " + t3.isAlive());
+
+        c.join();
+        t1.join();
+        t3.join();
+
+        System.out.println(" --- "+Thread.currentThread().getName()+" --- ");
+
+    }
+
+
+}
