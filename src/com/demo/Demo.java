a0d7a5a38ce7b32665663b0612f347bdc463eaa1
Naveen Prasanna <naveenprasannavnp@gmail.com>
2025-11-11 11:01:31 +0530

    updated code

--- src/com/demo/Demo.java
+++ src/com/demo/Demo.java
@@ -1,78 +1,38 @@
 package com.demo;
 
-class Car extends Thread {
-    public String createCar(int i) {
-        return "Car is running : " + i;
-    }
+class Mythread extends Thread{
+
+    int count;
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(createCar(i));
+        System.out.println(Thread.currentThread().getName()+" : is running");
+        synchronized (this){
+            System.out.println(Thread.currentThread().getName()+" : is running inside Sync this");
+            for (int i = 0; i < 100; i++) {
+                count++;
+            }
+            this.notify();
+            System.out.println(Thread.currentThread().getName()+" : is exit from sync this");
         }
+        System.out.println(Thread.currentThread().getName()+" : is exit");
     }
 }
 
-class Bus implements Runnable {
+public class Demo{
+    static void main(String[] args) throws InterruptedException {
+        Mythread mt = new Mythread();
+        mt.start();
 
-    public String createBus(int i) {
-        return "Bus is running : " + i;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(createBus(i));
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                // Restore interrupt status and exit loop (cooperative interruption)
-                Thread.currentThread().interrupt();
-                break;
-            }
+        System.out.println(Thread.currentThread().getName()+" : is running in main method");
+        synchronized (mt){
+            System.out.println(Thread.currentThread().getName()+" : is running inside Sync mt");
+            mt.wait();
+            System.out.println("COUNT IS: "+mt.count);
+            System.out.println(Thread.currentThread().getName()+" : is exit from Sync mt");
         }
-    }
-}
+        System.out.println(Thread.currentThread().getName()+" : is exit from main method");
 
-public class Demo {
-    public static void main(String[] args) throws InterruptedException {
 
-        System.out.println(" --- " + Thread.currentThread().getName() + " --- ");
-
-        Car c = new Car();
-        c.setName("Thread-1");
-        c.start();
-
-        System.out.println("Thread : " + c.getName() + ", is alive : " + c.isAlive());
-
-        // yield is a static hint to the scheduler
-        Thread.yield();
-        System.out.println("Thread : " + c.getName() + ", is alive : " + c.isAlive());
-
-        Thread t1 = new Thread(new Bus());
-        t1.setName("Thread-2");
-        t1.start();
-
-        System.out.println("Thread : " + t1.getName() + ", is alive : " + t1.isAlive());
-        Thread.yield();
-        System.out.println("Thread : " + t1.getName() + ", is alive : " + t1.isAlive());
-
-        Thread t3 = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                System.out.println("Lorry is running : " + i);
-            }
-        }, "Thread-3");
-
-        t3.start();
-        System.out.println("Thread : " + t3.getName() + ", is alive : " + t3.isAlive());
-        Thread.yield();
-        System.out.println("Thread : " + t3.getName() + ", is alive : " + t3.isAlive());
-
-        // Wait for all threads to finish
-        c.join();
-        t1.join();
-        t3.join();
-
-        System.out.println(" --- " + Thread.currentThread().getName() + " --- ");
     }
 }
